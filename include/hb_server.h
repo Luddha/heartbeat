74456039713a7eb8ff9b599176064ed70cc53cc6
marvin-leister <tuxbuddha@gmail.com>
2019-04-29 20:37:31 -0500

    started some files and notes

---
+++ include/hb_server.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/*
+ *
+ * add struct for beat
+ * add function for processing 
+ * possibly add options for ncurses view, most likely add to a separate header
+ *
+ *
+ */
---
+++ src/heartbeat.c
@@ -0,0 +1,45 @@
+// main file for heartbeat
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <string.h>
+
+
+//#include "common.h"
+//#include "hb_server.h"
+
+#define PORT 1337
+
+
+int main(int argc, char **argv){
+
+	// add gotopt for input
+	// options,
+	// 		server mode
+	// 		client mode
+	// 		ip address
+	// 		port number
+	// add header and src files for common functions
+	// add hb_server header and src files
+	// 	has struct
+
+	
+/*	
+	int sockfd;
+	struct sockaddr_in server;
+
+	sockfd = socket(PF_INET, SOCK_STREAM, 0);
+
+	server.sin_family = AF_INET;
+	server.sin_port = htons(PORT);
+	inet_aton(ip, &server.sin_addr);
+
+	*/
+
+
+}
